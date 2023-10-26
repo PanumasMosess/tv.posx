b9e19355b997674019bedf96e24f7f75fee5511a
Panumas Srisook <Panumas.srisook@gmail.com>
2023-10-26 12:58:12 +0700

    video

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -11,6 +11,6 @@ $routes->get('/ig_get/(:any)', 'DashBoardController::IG/$1');
 $routes->get('/ig_status/(:any)', 'DashBoardController::ig_status_update/$1');  
 $routes->get('/time_get/(:any)', 'DashBoardController::IG_TIME/$1');   
 $routes->get('/upload/(:any)', 'UploadController::index/$1');
-$routes->get('/upload_video/(:any)', 'UploadController::upload_video/$1');
 $routes->get('/upload_img/(:any)', 'UploadController::upload_img/$1');
+$routes->get('/upload_video/(:any)', 'UploadController::upload_video/$1');
 $routes->post('/upload_customer', 'UploadController::inert_message');
